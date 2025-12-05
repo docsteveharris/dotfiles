1ab3f24852d2aa630ffc08510b47deebd8477166
Steve Harris <doc@steveharris.me>
2025-12-05 15:48:03 +0000

    adding juliaformatter

---
+++ config/nvim/lua/plugins/juliaformatter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "kdheepak/JuliaFormatter.vim",
+    ft = "julia", -- load only for Julia files
+    -- keys = {
+    --   -- example via http://www.lazyvim.org/configuration/plugins#%EF%B8%8F-adding--disabling-plugin-keymaps
+    --   -- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    --   { "<leader>jf", "<cmd>JuliaFormatter  JuliaFormatterFormat<cr>", desc = "Julia Format" },
+    -- },
+  },
+}
