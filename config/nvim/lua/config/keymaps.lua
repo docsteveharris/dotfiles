0cbf073326a3c0cc35e6b67f1bed2c56c3aa0664
Steve Harris <doc@steveharris.me>
2026-01-30 08:18:48 +0000

    multiple minor updates on mac

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- lazyvim space fw saves the file
+vim.keymap.set("n", "<leader>fw", "<cmd>update<cr>", {
+  desc = "Save file",
+})
---
+++ macos/defaults-mailmate.sh
@@ -0,0 +1,3 @@
+# https://manual.mailmate-app.com/hidden_preferences
+defaults write com.freron.MailMate MmSendMessageDelayEnabled -bool YES
+defaults write com.freron.MailMate MmSendMessageDelayString -string "15 seconds"
